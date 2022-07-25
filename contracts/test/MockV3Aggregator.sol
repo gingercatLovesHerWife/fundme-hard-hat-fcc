d3026b1217276f2778be3876a997664d83fbd11d
mac <mac@macs-MacBook-Pro.local>
2022-07-25 09:07:44 +0800

    2nd commit

---
+++ contracts/test/MockV3Aggregator.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+// import "../node_modules/@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol";
---
+++ deploy/99-deploy-storage-fun.js
@@ -0,0 +1,29 @@
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("----------------------------------------------------")
+    log("Deploying FunWithStorage and waiting for confirmations...")
+    const funWithStorage = await deploy("FunWithStorage", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    log("Logging storage...")
+    for (let i = 0; i < 10; i++) {
+        log(
+            `Location ${i}: ${await ethers.provider.getStorageAt(
+                funWithStorage.address,
+                i
+            )}`
+        )
+    }
+}
+
+module.exports.tags = ["storage"]
